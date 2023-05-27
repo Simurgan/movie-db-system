a86204c10f7e6a442ec5acb3db7a7b6fbbb2c7b6
uyduranomer <uyduranomersukru@gmail.com>
2023-05-27 14:52:44 +0300

    implement sign-in and admin front-end pages

--- main.py
+++ main.py
@@ -5,13 +5,92 @@ app = Flask(__name__)
 
 # Create a rouote decorator
 @app.route('/')
-
 def index():
     return render_template("index.html")
 
 @app.route('/signin/')
 def sign_in():
-    return "<h1>Sign-in Page</h1>"
+    return render_template("signin.html")
+
+@app.route('/admin/')
+def admin():
+    content = {
+        "movieRating": {
+            "movieID": 0,
+            "movieName": "Inception",
+            "overallRating": 4.5
+        },
+        "movies": {
+            "director": "director1",
+            "movieList": [
+                {
+                    "movieID": 0,
+                    "movieName": "The Departed",
+                    "theatreID": 5,
+                    "district": "New York",
+                    "timeSlot": 2
+                },
+                {
+                    "movieID": 4,
+                    "movieName": "The Wolf of The Wall Street",
+                    "theatreID": 7,
+                    "district": "Arizona",
+                    "timeSlot": 3
+                },
+                {
+                    "movieID": 9,
+                    "movieName": "Goodfellas",
+                    "theatreID": 9,
+                    "district": "Coralado",
+                    "timeSlot": 1
+                }
+            ]
+        },
+        "userRating": {
+            "username": "audience_1",
+            "ratings": [
+                {
+                    "movieID": 0,
+                    "movieName": "Hateful Eight",
+                    "rating": 4.9
+                },
+                {
+                    "movieID": 1,
+                    "movieName": "Pulp Fiction",
+                    "rating": 4.9
+                },
+                {
+                    "movieID": 2,
+                    "movieName": "Django: Unchained",
+                    "rating": 4.8
+                }
+            ]
+        },
+        "directorList": [
+            {
+                "username": "director1",
+                "name": "director_name1",
+                "surname": "director_surname1",
+                "nation": "director_nationality1",
+                "platform_id": "director_platformid1"
+            },
+            {
+                "username": "director2",
+                "name": "director_name2",
+                "surname": "director_surname2",
+                "nation": "director_nationality2",
+                "platform_id": "director_platformid2"
+            },
+            {
+                "username": "director3",
+                "name": "director_name3",
+                "surname": "director_surname3",
+                "nation": "director_nationality3",
+                "platform_id": "director_platformid3"
+            }
+        ]
+    }
+    return render_template("admin.html", content=content)
 
 @app.route('/instance_user_page/<name>')
 def instance_user_page(name):

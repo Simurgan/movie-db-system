de54352a17d1fb11e4218d0832b4cbaef49f767a
uyduranomer <uyduranomersukru@gmail.com>
2023-05-29 04:07:19 +0300

    back-end and some database

--- db.py
+++ db.py
@@ -5,15 +5,28 @@ class database:
     self.db = mysql.connector.connect(
       host="localhost",
       user="root",
-      passwd = "password123",
-      database="movie_db"
+      passwd="123456",
+      database="moviedbdb"
     )
 
     self.cursor = self.db.cursor()
 
-  def execute(self, query, params):
-    self.cursor.execute(query, params)
-    return self.cursor.fetchone()
+  def execute(self, query):
+    try:
+      self.cursor.execute(query)
+      return self.cursor.fetchone()
+    except Exception as ex:
+      print("execute error:")
+      print(ex)
+      return False
+
+  def save(self):
+    try:
+      self.db.commit()
+      return True
+    except:
+      print("save error")
+      return False
 
   def close(self):
     self.cursor.close()
